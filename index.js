9e19ec1a714d3b54dc15d63efb1a1a969df19d79
Han Qing <zhaohanqing96@gmail.com>
2021-04-11 02:16:00 +0800

    Update build folder path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, './build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './build', 'index.html'));
